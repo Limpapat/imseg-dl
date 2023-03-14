2e1528340b5d342f0458985617712ae073d0d182
Limpapat Bussaban <lim.bussaban@gmail.com>
2023-03-14 20:30:24 +0700

    .

--- imsegdl/eval.py
+++ imsegdl/eval.py
@@ -61,6 +61,10 @@ def eval(params:dict):
     saving_path = os.path.join(RESULT_PATH, "evaluation_{}".format(datetime.now().strftime("%Y%m%d%H%M%S")))
     os.mkdir(saving_path)
 
+    print("-"*40)
+    print("Filter with probability : {}".format(P))
+    print("-"*40)
+
     # evaluation
     model.eval()
     anns={"last_ann_id":-1, "annotation":[]}
