5c4e2dde264c0629f7bc8ad36fef1bde9c822fd9
Limpapat Bussaban <lim.bussaban@gmail.com>
2023-05-13 21:38:03 +0700

    .

--- imsegdl/plots.py
+++ imsegdl/plots.py
@@ -33,7 +33,7 @@ def plot_test_gt(ds:COCODataset, gt_ds:COCODataset)->dict:
         anns = gt_ds.coco.loadAnns(ann_ids)
         fig2, ax2 = plt.subplots(figsize=(10,10))
         ax2.imshow(Image.open(f'{gt_ds.root_dir}/{img["file_name"]}').convert('RGB'))
-        gt_ds.coco.showAnns(anns, draw_bbox=True)
+        gt_ds.coco.showAnns(anns, draw_bbox=False)
         plt.title(f"======= {gt_ds.samples(_mapping[idx])} =======")
     plt.show()
     return _mapping
