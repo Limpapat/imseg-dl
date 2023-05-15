54558d9501cea406ec730ade23d9f2085892c9d6
Limpapat Bussaban <lim.bussaban@gmail.com>
2023-05-15 11:49:13 +0700

    .

--- imsegdl/eval.py
+++ imsegdl/eval.py
@@ -84,7 +84,7 @@ def eval(params:dict):
             X, y = X.to(DEVICE), y.to(DEVICE)
             #####
             y_detach = y.detach().cpu()
-            y_detach = y_detach[:,1,:,:]
+            y_detach = y_detach[:,2,:,:]
             print(y_detach[y_detach > 0])
             # y_plot = torch.zeros([1, 1, y_detach.shape[-2], y_detach.shape[-1]])
             # for i in range(6):
