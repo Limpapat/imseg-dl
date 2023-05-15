53ab7e621b055b1f9b7b23896e74ed7b6be9b42e
Limpapat Bussaban <lim.bussaban@gmail.com>
2023-05-15 11:26:06 +0700

    .

--- imsegdl/eval.py
+++ imsegdl/eval.py
@@ -103,8 +103,7 @@ def eval(params:dict):
                 y_plot = torch.zeros([BATCH_SIZE, 1, y_detach.shape[-2], y_detach.shape[-1]])
                 for i in range(N_CLASSES):
                     y_plot += i*y_detach[:,i,:,:]
-                    y_plot += 1.
-                    print(i, y_plot[y_plot > 0])
+                    print(i, y_plot[y_plot > 0], y_detach[y_detach > 0])
                 plt.imshow(y_plot.squeeze().numpy())
                 plt.title("ground truth")
                 sp = plt.subplot(1, 2, 2)
