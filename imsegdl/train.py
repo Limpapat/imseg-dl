14916e0489743754858816836166d14f66bb6d25
Limpapat Bussaban <lim.bussaban@gmail.com>
2023-07-29 09:53:35 +0700

    .

--- imsegdl/train.py
+++ imsegdl/train.py
@@ -268,7 +268,7 @@ def train(params:dict):
     plt.savefig(f'{saving_path}/loss.png')
     if DISP_PLOT:
         plt.show()
-    return model_saving_path if not model_saving_path else os.path.join(saving_path, "no_model_saved")
+    return model_saving_path if not model_saving_path else PRETRAINED_MODEL
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
