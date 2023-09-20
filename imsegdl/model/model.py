b13cd68db407dab64e192b6d71b89a9af32ce16d
Limpapat Bussaban <lim.bussaban@gmail.com>
2023-09-20 10:36:03 +0700

    done

--- imsegdl/model/model.py
+++ imsegdl/model/model.py
@@ -134,7 +134,6 @@ class CNN(nn.Module):
     
     def forward(self, x):
         x = self.fe(x)
-        print(x.size())
-        x = x.view(-1, 64 * 33 * 33)
+        x = x.view(-1, 64 * 31 * 31)
         logits = self.classifier(x)
         return logits
